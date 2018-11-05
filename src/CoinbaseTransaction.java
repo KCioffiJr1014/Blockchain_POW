ef259ed0468bb6dd09777ba229a30ea4029d5795
KCioffiJr1014 <KCioffiJr1014@gmail.com>
2018-11-05 15:53:15 -0500

    .

---
+++ src/CoinbaseTransaction.java
@@ -0,0 +1,16 @@
+public class CoinbaseTransaction {
+    private String hash;
+    private String output;
+    private int reward;
+
+    public CoinbaseTransaction() {
+        this.hash = null;
+        this.output = Long.toHexString(0xffffffff);
+        this.reward = reward;
+
+    }
+
+
+
+
+}
