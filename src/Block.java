5a634838ac5538c12e74b0944078854d3a945091
KCioffiJr1014 <KCioffiJr1014@gmail.com>
2018-11-05 14:50:50 -0500

    Works as needed

--- src/Block.java
+++ src/Block.java
@@ -2,29 +2,75 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
-    private String hash;
-    private String input;
-    private String previousHash;
+    public String hash;
+    private String data;
+    public String previousHash;
+    //public CoinbaseTransaction coinbaseTransaction;
     private ArrayList transactions;
-    //private blockHeader headerData;
     private long timestamp;
-    private int noonce;
+    private int nonce;
 
+    /**
+     * @param data - input source for encryption
+     * @param previousHash - previous encrypted hash
+     * @field hash - encrypts all data going into the block
+     * @field timestamp - stamps a date
+     * @field transactions - all the transactions within the block
+     */
     public Block(String data, String previousHash) {
         this.transactions = new ArrayList();
-        //this.headerData = headerData;
-        this.hash = calculateHash();
+        this.data = data;
         this.previousHash = previousHash;
         this.timestamp = new Date().getTime();
+        //this.coinbaseTransaction = new CoinbaseTransaction();
+        this.hash = calculateHash(); // To be added after all fields
     }
 
+    public String getHash() {
+        return hash;
+    }
+
+
+    /**
+     * Calculate hash
+     * @return encrypted hash for blocks
+     */
     public String calculateHash() {
         String calculatedhash = StringEncrypt.applySHA256(
                 previousHash +
                         Long.toString(timestamp) +
-                        input
+                        data
         );
         return calculatedhash;
     }
 
+    /**
+     * mineBlock
+     * @param difficulty
+     *
+     * 1. Takes hash and generates a target value
+     * 2. Any null values will be replaced with zeros
+     * 3. The algorithm will begin counting number of nonces
+     */
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
+
+    /**
+    public String toString() {
+        String store = "";
+        store += "Transactions: " + this.transactions + "\n";
+        store += "Input data: " + this.data + "\n";
+        store += "Hash output: " + this.hash + "\n";
+        store += "Previous Hash: " + this.previousHash + "\n";
+        store += "Generation time: " + this.timestamp;
+        return store;
+    }
+     */
 }
